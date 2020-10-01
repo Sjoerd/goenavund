f69b5e74ea2fc54e8da8ed232518a8ddcd39bd9a
Sjoerd <info@sjoerdk.nl>
2020-10-01 21:47:09 +0200

    design

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,37 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="bg-gray-300 min-h-screen bg-cover">
+    <div class="md:max-w-3xl px-3 pb-8 mx-auto">
+      
+      <Logo />
+
+
+      <transition
+        enter-active-class="transition-all transition-75 ease-out"
+        leave-active-class="transition-all transition-100 ease-in"
+        enter-class="opacity-0 scale-70"
+        enter-to-class="opacity-100 scale-100"
+        leave-class="opacity-100 scale-100"
+        leave-to-class="opacity-0 scale-70"
+      >
+        <div class="sm:grid grid-cols-3 md:grid-cols-3 gap-4">
+          <SoundButton v-for="i in 15" :key="i" />
+        </div>
+      </transition>
+
+
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import SoundButton from './components/SoundButton.vue'
+import Logo from './components/Logo.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Logo,
+    SoundButton
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/SoundButton.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="block shadow-md hover:shadow-lg mb-3 md:mb-0 bg-white p-4 font-sans cursor-pointer text-center">
+    <span>Goenavund</span>
+  </div>
+</template>
